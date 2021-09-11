1f47fdf646606ce6278617da28c6bcf9ae7d24b3
Vlpavel <88313525+Vlpavel@users.noreply.github.com>
2021-09-11 20:25:11 +0300

    Add files via upload

---
+++ Weather v2/Weather v2/Base/BaseVievStoryboard.swift
@@ -0,0 +1,25 @@
+//
+//  BaseVievStoryboard.swift
+//  Weather v2
+//
+//  Created by Павел Власов on 11.09.2021.
+//
+
+import UIKit
+
+
+class BaseVievStoryboard: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+class orangeStory: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+        
+}
+
---
+++ Weather v2/Weather v2/Register/ViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  Weather v2
+//
+//  Created by Павел Власов on 11.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var LoginTextField: UITextField!
+    @IBOutlet weak var PasswordTextField: UITextField!
+    
+     override func viewDidLoad() {
+         super.viewDidLoad()
+         addGesture()
+     }
+
+     override func viewWillAppear(_ animated: Bool) {
+         super.viewWillAppear(animated)
+         self.addObsservers()
+     }
+     
+     override func viewWillDisappear(_ animated: Bool) {
+         super.viewWillDisappear(animated)
+         self.removeObservers()
+     }
+     
+     private func addGesture() {
+         let gesture = UITapGestureRecognizer(target: self, action: #selector(handleGesture))
+         self.scrollView.addGestureRecognizer(gesture)
+     }
+     @objc private func handleGesture() {
+         self.scrollView.endEditing(true)
+     }
+     private func addObsservers() {
+         NotificationCenter.default
+             .addObserver(self,
+                          selector: #selector(handleKeyboardWillShow),
+                          name: UIResponder.keyboardWillShowNotification,
+                          object: nil)
+         NotificationCenter.default
+             .addObserver(self,
+                          selector: #selector(handleKeyboardWillHide),
+                          name: UIResponder.keyboardWillHideNotification,
+                          object: nil)
+     }
+
+     private func removeObservers() {
+         NotificationCenter.default
+             .removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+         
+         NotificationCenter.default
+             .removeObserver(self,
+                             name: UIResponder.keyboardWillHideNotification,
+                             object: nil)
+     }
+     
+     @objc private func handleKeyboardWillShow() {
+         self.scrollView.contentInset.bottom += 120
+     }
+     
+     @objc private func handleKeyboardWillHide() {
+         self.scrollView.contentInset.bottom = 0
+
+     }
+     
+     private func checkPassword() {
+             let login = self.LoginTextField.text
+             let password = self.PasswordTextField.text
+         
+             if login == "admin" && password == "123456" {
+                 self.showAdmin()
+             } else {
+                 self.showAlert()
+         }
+         
+     }
+     
+     private func showAdmin() {
+        let storyBoard = UIStoryboard(name: "Base", bundle: nil)
+        let viewController = storyBoard.instantiateInitialViewController()
+        if let viewController = viewController as? BaseVievStoryboard {
+            self.present(viewController, animated: true)
+        }
+     }
+     
+     private func showAlert() {
+     let alertController = UIAlertController(title: "Ошибка",
+        message: "Неверный логин/пароль",
+        preferredStyle: .alert)
+     let action = UIAlertAction(title: "OK",
+                                style: .cancel)
+         alertController.addAction(action)
+         self.present(alertController, animated: true)
+     }
+     @IBAction private func OnButtonPressed(_ sender: Any) {
+         self.checkPassword()
+     }
+
+ }
